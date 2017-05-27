2a49af93a32e7475256691f22b9cf9de0ece1919
zkfy <lib4net@163.com>
2017-05-27 10:19:20 +0800

    添加mq代码

--- context.mq.go
+++ context.mq.go
@@ -2,10 +2,33 @@ package goplugin
 
 import (
 	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/qxnw/lib4go/jsons"
 	"github.com/qxnw/lib4go/mq"
 )
+
+//SendMQMessage 发送MQ消息
+func (w *PluginContext) SendMQMessage(queueName string, msg string, timeoutName string) error {
+	if w.Args[queueName] == "" || w.Args[timeoutName] == "" {
+		return fmt.Errorf("输入参数为空:%s或%s", queueName, timeoutName)
+	}
+	timeout, err := strconv.Atoi(w.Args[timeoutName])
+	if err != nil {
+		return fmt.Errorf("MQ_TIMEOUT转换为数字失败.mq_timeout:%s", w.Args[timeoutName])
+	}
+	mqProducer, err := w.GetMQProducer()
+	if err != nil {
+		return fmt.Errorf("初始化MQ对象失败(err:%v)", err)
+	}
+	err = mqProducer.Send(w.Args[queueName], msg, time.Duration(timeout))
+	if err != nil {
+		return fmt.Errorf("发送MQ失败.队列名称:%s,消息内容:%s", w.Args[queueName], msg)
+	}
+	return nil
+
+}
 
 //GetMQProducer 获取GetMQProducer
 func (w *PluginContext) GetMQProducer() (p mq.MQProducer, err error) {
