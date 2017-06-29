f116f0dbc2adae7ca0b24b145ba1f580b341f7b7
zkfy <lib4net@163.com>
2017-06-29 16:59:48 +0800

    修改rpc调用名称

--- context.rpc.go
+++ context.rpc.go
@@ -6,14 +6,14 @@ import (
 	"github.com/qxnw/lib4go/jsons"
 )
 
-//RPCRquest RPC请求
-func (w *PluginContext) RPCRquest(service string, input map[string]string, failFast bool) (status int, r string, param map[string]string, err error) {
+//RPCRequest RPC请求
+func (w *PluginContext) RPCRequest(service string, input map[string]string, failFast bool) (status int, r string, param map[string]string, err error) {
 	status, r, param, err = w.RPC.Request(service, input, failFast)
 	return
 }
 
-//RPCRquestMap RPC请求返回结果转换为map
-func (w *PluginContext) RPCRquestMap(service string, input map[string]string, failFast bool) (status int, r map[string]interface{}, param map[string]string, err error) {
+//RPCRequestMap RPC请求返回结果转换为map
+func (w *PluginContext) RPCRequestMap(service string, input map[string]string, failFast bool) (status int, r map[string]interface{}, param map[string]string, err error) {
 	status, rx, param, err := w.RPC.Request(service, input, failFast)
 	if err != nil || status != 200 {
 		err = fmt.Errorf("rpc请求(%s)失败:%d,err:%v", service, status, err)
