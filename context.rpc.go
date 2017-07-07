5b9bf36edad40f663de8cb018662f502ef15798b
zkfy <lib4net@163.com>
2017-07-07 16:54:36 +0800

    添加RPC失败重试机制

--- context.rpc.go
+++ context.rpc.go
@@ -16,6 +16,16 @@ func (cr *ContextRPC) Reset(ctx *PluginContext) {
 	cr.ctx = ctx
 }
 
+//RequestFailRetry RPC请求
+func (cr *ContextRPC) RequestFailRetry(service string, input map[string]string, times int) (status int, r string, param map[string]string, err error) {
+	status, r, param, err = cr.ctx.rpc.RequestFailRetry(service, input, times)
+	if err != nil || status != 200 {
+		err = fmt.Errorf("rpc请求(%s)失败:%d,err:%v", service, status, err)
+		return
+	}
+	return
+}
+
 //Request RPC请求
 func (cr *ContextRPC) Request(service string, input map[string]string, failFast bool) (status int, r string, param map[string]string, err error) {
 	status, r, param, err = cr.ctx.rpc.Request(service, input, failFast)
