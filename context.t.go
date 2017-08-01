d6a2c7f0154f6c7ed2ca98480289e52d40f72192
zkfy <lib4net@163.com>
2017-08-01 19:53:23 +0800

    添加测试类

---
+++ context.t.go
@@ -0,0 +1,51 @@
+package goplugin
+
+import (
+	"github.com/qxnw/lib4go/transform"
+	"github.com/qxnw/lib4go/utility"
+)
+
+type TContext struct {
+	Input      transform.ITransformGetter
+	Params     transform.ITransformGetter
+	Args       map[string]string
+	Body       string
+	Ext        map[string]interface{}
+	FuncVarGet func(c string, n string) (string, error)
+}
+
+func (g *TContext) GetInput() transform.ITransformGetter {
+	return g.Input
+
+}
+func (g *TContext) GetParams() transform.ITransformGetter {
+	return g.Params
+
+}
+func (g *TContext) GetArgs() map[string]string {
+	return g.Args
+}
+func (g *TContext) GetBody() string {
+	return g.Body
+
+}
+func (g *TContext) GetExt() map[string]interface{} {
+	return g.Ext
+}
+
+//NewTContext 构建用于测试的context对象
+func NewTContext() *TContext {
+	t := &TContext{}
+	t.Input = transform.New().Data
+	t.Params = transform.New().Data
+	t.Ext = map[string]interface{}{
+		"__func_var_get_": func(c string, n string) (string, error) {
+			if t.FuncVarGet == nil {
+				return "", nil
+			}
+			return t.FuncVarGet(c, n)
+		},
+		"hydra_sid": utility.GetGUID()[0:8],
+	}
+	return t
+}
